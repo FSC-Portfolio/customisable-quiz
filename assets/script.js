4666fb017d314a73309902162d794a2c582f9c7d
JayArghArgh <jayarghargh@gmail.com>
2020-12-02 15:41:16 +1000

    loaded the questions and printed to console

--- assets/script.js
+++ assets/script.js
@@ -8,8 +8,17 @@ function setDefault() {
 function loadData() {
     if(!localStorage.getItem("qanda")) {
         console.log("empty storage");
+        // it's the first time playing so set the default
         setDefault();
+    } else {
+        // Load the questions
+        var loadedQuestions = JSON.parse(localStorage.getItem("qanda"));
+        loadedQuestions.forEach(function (question, index) {
+            console.log(index, question.title);
+        })
+        console.log("load status...")
     }
+    // otherwise load the existing data.
 }
 
 loadData();
