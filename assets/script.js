b230d50ef130213b5a3000e890043d35bf82600c
JayArghArgh <jayarghargh@gmail.com>
2020-12-02 16:21:49 +1000

    created and tested array shuffler

--- assets/script.js
+++ assets/script.js
@@ -5,20 +5,31 @@ function setDefault() {
     // $('#main-text').html(JSON.stringify(questions));
 }
 
+function shuffleArray(arrayToShuffle) {
+    // Shuffles whatever array is parsed to it.
+    for(var i = arrayToShuffle.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * i)
+        var temp = arrayToShuffle[i];
+        arrayToShuffle[i] = arrayToShuffle[j];
+        arrayToShuffle[j] = temp;
+    }
+    // Return shuffled result.
+    return arrayToShuffle;
+}
+
 function loadData() {
     if(!localStorage.getItem("qanda")) {
         console.log("empty storage");
         // it's the first time playing so set the default
         setDefault();
     } else {
-        // Load the questions
-        var loadedQuestions = JSON.parse(localStorage.getItem("qanda"));
-        loadedQuestions.forEach(function (question, index) {
-            console.log(index, question.title);
+        // Load the questions and shuffle them.
+        var loadedQuestions = shuffleArray(JSON.parse(localStorage.getItem("qanda")));
+        loadedQuestions.forEach(function (item) {
+            console.log(item);
         })
-        console.log("load status...")
+
     }
-    // otherwise load the existing data.
 }
 
 loadData();
