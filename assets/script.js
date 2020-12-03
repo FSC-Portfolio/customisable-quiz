5da08b83e1e893380abdc5469b6a8686986053b1
JayArghArgh <jayarghargh@gmail.com>
2020-12-03 15:23:05 +1000

    got a play through on all of the questions.

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 "use strict";
+var loadedQuestions;
+var currentQuestion = 0;
 
 function shuffleArray(arrayToShuffle) {
     // Shuffles whatever array is parsed to it.
@@ -12,27 +14,90 @@ function shuffleArray(arrayToShuffle) {
     return arrayToShuffle;
 }
 
-function setDefault() {
-    // Shuffle and store the questions.
-    localStorage.setItem("qanda", JSON.stringify(shuffleArray(questions)));
-    // $('#main-text').html(JSON.stringify(questions));
-}
-
 function loadData() {
     if(!localStorage.getItem("qanda")) {
         console.log("empty storage");
         // it's the first time playing so set the default
-        setDefault();
+        localStorage.setItem("qanda", JSON.stringify(shuffleArray(questions)));
     } else {
         // Load the questions.
-        var loadedQuestions = JSON.parse(localStorage.getItem("qanda"));
+        loadedQuestions = JSON.parse(localStorage.getItem("qanda"));
         // loadedQuestions.forEach(function (item) {
         //     console.log(item);
         // })
     }
 }
 
+function playGame() {
+    $('#main-text').html(loadedQuestions[currentQuestion].title);
+    var answerList = $('#answer-list');
+    answerList.html("");
+    var actualAnswer = loadedQuestions[currentQuestion].answer;
+    $(loadedQuestions[currentQuestion].choices).each(function (index, item) {
+        var choiceDivRow = $('<div>');
+        choiceDivRow.attr("class", "row");
+        var choiceDivCol = $('<div>');
+        choiceDivCol.attr("class", "col-md-12");
+        choiceDivCol.html('<button type="button" id="'+ item +'" class="btn btn-primary btn-lg">' + item +'</button>');
+        choiceDivRow.append(choiceDivCol);
+        $('#answer-list').append(choiceDivRow);
+    })
+
+
+
+    $('#answer-list').click(function(event){
+        event.preventDefault();
+        // make sure we're clicking on a button
+        if (event.target.matches(".btn")) {
+            // check the result
+            if (event.target.id === actualAnswer) {
+                console.log("okies");
+                currentQuestion += 1;
+                playGame();
+            } else {
+                console.log("nokies");
+            }
+        }
+    })
+}
+
+
+// <div class="row">
+//     <div class="col-md-12">
+//         <button type="button" class="btn btn-primary btn-lg">answer</button>
+//     </div>
+// </div>
+//
+// <div className="row">
+//     <div className="col">
+//         <button type="button" className="btn btn-primary btn-lg">answer</button>
+//     </div>
+// </div>
+//
+// <div className="row">
+//     <div className="col">
+//         <button type="button" className="btn btn-primary btn-lg">answer</button>
+//     </div>
+// </div>
+//
+// <div className="row">
+//     <div className="col">
+//         <button type="button" className="btn btn-primary btn-lg">answer</button>
+//     </div>
+// </div>
+    // display the first question
+    // display the answers
+    // when someone clicks an answer
+    // validate against known answer
+    // if a wrong question is selected subtract 10 seconds from the timer.
+    // if a correct answer is selected move to next question
+    // when completed
+    // stop the clock
+    // ask for initials to go on high score page
+
 function theCounter(countdownSeconds) {
+    // This is the countdown timer.
+    // TODO needs to return a flag when started to stop user restarting!
     var counter = countdownSeconds;
     var interval = setInterval(function () {
         counter--;
@@ -47,8 +112,8 @@ function theCounter(countdownSeconds) {
 }
 
 $('#btn-start').click(function () {
-    console.log("heya");
     theCounter(60);
+    playGame();
 });
 
 loadData();
